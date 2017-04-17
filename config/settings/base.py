e82c646ce04a677e0ad3d8606833b594e12983c2
Denis Taranov <taranov.denis.95@gmail.com>
2017-04-17 23:45:07 +0300

    added allowed host

--- config/settings/base.py
+++ config/settings/base.py
@@ -287,7 +287,7 @@ ADMIN_URL = r'^admin/'
 # ------------------------------------------------------------------------------
 
 #Django Telegram Bot settings
-TELEGRAM_BOT_TOKENS = ('345392316:AAGmMZBBY8pvMM9KpeOJoeoNC-FNVvTvYQI',)
+TELEGRAM_BOT_TOKEN = '345392316:AAGmMZBBY8pvMM9KpeOJoeoNC-FNVvTvYQI'
 TELEGRAM_WEBHOOK_SITE = 'https://178.62.212.110'
 TELEGRAM_WEBHOOK_BASE = '/ordersbot'
 TELEGRAM_WEBHOOK_CERTIFICATE = 'webhook_cert.pem' #If your site use self-signed certificate, must be set with location of your public key certificate. (More info at https://core.telegram.org/bots/self-signed )
