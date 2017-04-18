c2070062848b996d17e4b480acaadd6d9223d669
Denis Taranov <taranov.denis.95@gmail.com>
2017-04-18 03:29:53 +0300

    added order model

--- tinkoff_order_bot/orders/admin.py
+++ tinkoff_order_bot/orders/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from tinkoff_order_bot.orders.models import Order
+
+def update_status(status, queryset):
+    queryset.update(status=status)
+
+
+def make_confirmed(modeladmin, request, queryset):
+    update_status(Order.CONFIRMED, queryset)
+make_confirmed.short_description = "Mark selected orders as confirmed"
+
+
+def make_rejected(modeladmin, request, queryset):
+    update_status(Order.REJECTED, queryset)
+make_rejected.short_description = "Mark selected orders as rejected"
+
+
+def make_nonconfirmed(modeladmin, request, queryset):
+    update_status(Order.NON_CONFIRMED, queryset)
+make_nonconfirmed.short_description = "Mark selected orders as non-confirmed"
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['number', 'status']
+    ordering = ['number']
+    actions = [make_confirmed, make_rejected, make_nonconfirmed]
+
+admin.site.register(Order, OrderAdmin)
--- tinkoff_order_bot/orders/models.py
+++ tinkoff_order_bot/orders/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+from model_utils import choices
+
+
+class Order(models.Model):
+    number = models.AutoField(primary_key=True)
+
+    CONFIRMED = 'C'
+    NON_CONFIRMED = 'NC'
+    REJECTED = 'RJ'
+
+    STATUS_CHOICES = (
+        (CONFIRMED, 'Confirmed'),
+        (NON_CONFIRMED, 'Non confirmed'),
+        (REJECTED, 'Rejected'),
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_CHOICES,
+        default=NON_CONFIRMED,
+    )
+
+    def __str__(self):
+        return 'Order {id}:{status}'.format(id=self.number, status=self.status)
+
